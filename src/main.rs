5e65f4ee1fdb9658e5b48be845f5f2f29a47d88d
Skylar Broad <skylar.broad@gmail.com>
2020-10-03 13:13:10 -0700

    p1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", p1(1000));
+}
+
+fn p1(max: u32) -> u32 {
+    (1..max).filter(|x| x % 3 == 0 || x % 5 == 0).sum()
+}
+
+#[test]
+fn test_p1() {
+    assert_eq!(p1(1000), 233168)
 }
